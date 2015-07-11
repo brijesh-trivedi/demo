875a6559915099baf3d470814ae65fc7cc66c68b
brijesh <brijesh.stepin@gmail.com>
2015-07-11 17:59:23 +0530

    add file

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-// Delete for confirmation box..
-$(document).ready(function() {
-	// Delete record on Id
-	$('.deleteLinkButton').click(function() {
-		var recordId = $(this).attr('id');
-		var anchor_tag_html = $('#anchor_' + recordId).html();
-		$('#delete_anchor_tag_content').html(anchor_tag_html);
-	});
-});
